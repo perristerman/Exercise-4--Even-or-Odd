fa8a790e24dd35843f995fc939d05e9f9bb8603b
perristerman <perri.sterman@gmail.com>
2020-07-27 17:21:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+playernumber = input("Pick a number 1-10 ")
+playernumber = float(playernumber)
+if playernumber == 1 or 3 or 5 or 7 or 9:
+  print("That number is odd")
+else:
+  print("That number is even")
